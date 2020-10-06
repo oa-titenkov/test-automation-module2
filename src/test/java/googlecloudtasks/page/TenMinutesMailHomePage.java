18f303e0d89031808ac9d7ed221d91fc85aa194d
sohumble <lightning07@yandex.ru>
2020-10-06 16:00:37 +0300

    changed accept button text

--- src/test/java/googlecloudtasks/page/TenMinutesMailHomePage.java
+++ src/test/java/googlecloudtasks/page/TenMinutesMailHomePage.java
@@ -26,7 +26,7 @@ public class TenMinutesMailHomePage extends AbstractPage {
     @FindBy(xpath = "//h3[contains(text(),'USD')]")
     private WebElement mailPrice;
 
-    @FindBy(xpath = "//button[contains(text(),'Agree')]")
+    @FindBy(xpath = "//button[contains(text(),'I accept')]")
     private WebElement acceptPrivacyButton;
 
 
@@ -43,7 +43,7 @@ public class TenMinutesMailHomePage extends AbstractPage {
         WebDriverWait wait = new WebDriverWait(driver, WAIT_TIMEOUT);
         wait.until(ExpectedConditions.attributeContains(emailAddress, "value", "@"));
         String emailAddressText = emailAddress.getAttribute("value");
-        if(driver.findElements(By.xpath("//button[contains(text(),'Agree')]")).size() != 0) {
+        if(driver.findElements(By.xpath("//button[contains(text(),'I accept')]")).size() != 0) {
             acceptPrivacyButton.click();
         }
         driver.switchTo().window(tabs.get(0));
@@ -55,7 +55,7 @@ public class TenMinutesMailHomePage extends AbstractPage {
         driver.switchTo().window(tabs.get(1));
         WebDriverWait wait = new WebDriverWait(driver, 30);
         wait.until(ExpectedConditions.textToBePresentInElement(inboxCounter, "1"));
-        if(driver.findElements(By.xpath("//button[contains(text(),'Agree')]")).size() != 0) {
+        if(driver.findElements(By.xpath("//button[contains(text(),'I accept')]")).size() != 0) {
             acceptPrivacyButton.click();
         }
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
