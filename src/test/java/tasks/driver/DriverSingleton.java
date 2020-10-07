75df9eb7df6a973ec8384c48e98c00a330c1acbc
Titenkov <navikx@gmail.com>
2020-10-07 20:04:32 +0300

    binary file location for chrome

--- src/test/java/tasks/driver/DriverSingleton.java
+++ src/test/java/tasks/driver/DriverSingleton.java
@@ -3,6 +3,7 @@ package tasks.driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverSingleton {
@@ -23,7 +24,9 @@ public class DriverSingleton {
                 }
                 case "chrome": {
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
+                    ChromeOptions options = new ChromeOptions();
+                    options.setBinary("c:\\Users\\Titenkov\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
+                    driver = new ChromeDriver(options);
                     break;
                 }
             }
