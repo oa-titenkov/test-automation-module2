45cb6a3a312d3b944340e081b32dea80aeb6d162
Titenkov <navikx@gmail.com>
2020-10-07 18:45:47 +0300

    added scroll for firefox

--- src/test/java/tasks/page/GoogleCloudCalculatorPage.java
+++ src/test/java/tasks/page/GoogleCloudCalculatorPage.java
@@ -1,14 +1,15 @@
 package tasks.page;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import tasks.model.ComputeEngine;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ResourceBundle;
 
 public class GoogleCloudCalculatorPage extends AbstractPage {
 
@@ -79,7 +80,9 @@ public class GoogleCloudCalculatorPage extends AbstractPage {
         getElement(computeEngine.getOperationSystemSoftware()).click();
         VMClassDropdown.click();
         getElement(computeEngine.getVMClass()).click();
-//        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", addToEstimateButton);
+        if(System.getProperty("browser").equals("firefox")) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", addToEstimateButton);
+        }
         wait.until(ExpectedConditions.visibilityOf(machineTypeDropdown)).click();
         getElement(computeEngine.getInstanceType()).click();
         if(!computeEngine.getGPUNumber().equals("")) {
