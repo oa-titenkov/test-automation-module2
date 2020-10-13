24f31b30a4119ef9bccccbf5fa94fe1885802ea0
Titenkov <navikx@gmail.com>
2020-10-13 19:59:03 +0300

    fixed io maintask again

--- src/main/java/learn/io/maintask/Runner.java
+++ src/main/java/learn/io/maintask/Runner.java
@@ -1,9 +1,6 @@
 package learn.io.maintask;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Objects;
@@ -14,17 +11,22 @@ public class Runner {
         File root = new File(args[0]);
         File file = new File("src\\main\\resources\\file_tree.txt");
         TreeNode node = new TreeNode(root);
-        if (root.isFile()) {
-            createDirectoryTree(root.getParentFile(), node);
-            System.out.println(node.getProperties());
+        if (root.getAbsolutePath().equals(file.getAbsolutePath())) {
+            System.out.println(parseFileTree());
         }
-        else  {
-            createDirectoryTree(root, node);
-        }
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-            writer.write(node.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
+        else {
+            if (root.isFile()) {
+                createDirectoryTree(root.getParentFile(), node);
+                System.out.println(node.getProperties());
+            }
+            else if (root.isDirectory())  {
+                createDirectoryTree(root, node);
+            }
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+                writer.write(node.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -39,6 +41,41 @@ public class Runner {
             }
         }
 
+    }
+
+    public static String parseFileTree() {
+        File file = new File("src\\main\\resources\\file_tree.txt");
+        int folderCount = 0;
+        int fileCount = 0;
+        double averageFileCount;
+        int allFileLength = 0;
+        double averageFileNameLength;
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                String trimmedLine = line
+                        .replaceAll("│", "")
+                        .replaceAll("\\|", "")
+                        .replaceAll("├───", "")
+                        .replaceAll("└───", "")
+                        .trim();
+                if (trimmedLine.split("\\.").length > 1) {
+                    fileCount++;
+                    allFileLength = allFileLength + trimmedLine.split("\\.")[0].length();
+                }
+                else folderCount++;
+            }
+            averageFileCount = (double) fileCount / folderCount;
+            averageFileNameLength = (double) allFileLength / fileCount;
+            return "Folders: " + folderCount + "\n" +
+                    "Files: " + fileCount + "\n" +
+                    "Average files in folder: " + String.format("%.2f", averageFileCount) + "\n" +
+                    "Average file name length: " + String.format("%.2f", averageFileNameLength) + "\n";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
     }
 
 }
