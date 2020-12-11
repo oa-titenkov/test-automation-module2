f3f4af93015228cf192f883b15bf5d4451920f79
oa-titenkov <navikx@gmail.com>
2020-12-11 12:18:07 +0300

    selenide implementation

---
+++ src/test/java/tasks/page/pastebinselenide/PastebinCreatePage.java
@@ -0,0 +1,51 @@
+package tasks.page.pastebinselenide;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
+import tasks.model.Paste;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PastebinCreatePage {
+
+    private final static String HOME_URL = "https://pastebin.com/";
+
+    private final SelenideElement codeArea = $(byId("postform-text"));
+    private final SelenideElement pasteExpirationContainer = $(byXpath("//span[@aria-labelledby='select2-postform-expiration-container']"));
+    private final SelenideElement syntaxHighlightingContainer = $(byXpath("//span[@aria-labelledby='select2-postform-format-container']"));
+    private final SelenideElement pasteName = $(byId("postform-name"));
+    private final SelenideElement createNewPasteButton = $(byXpath("//button[text()='Create New Paste']"));
+    private final SelenideElement syntaxHighlightingInput = $(byXpath("//input[@class='select2-search__field']"));
+    private final SelenideElement acceptPrivacyButton = $(byId("accept-choices"));
+
+
+    public PastebinCreatePage openPage() {
+        open(HOME_URL);
+        $(codeArea).shouldBe(Condition.visible);
+        return this;
+    }
+
+    public PastebinCreatedPage createPaste(Paste paste) {
+        $(codeArea).shouldBe(Condition.visible);
+
+        if (acceptPrivacyButton.is(Condition.visible)) {
+            acceptPrivacyButton.click();
+        }
+        codeArea.sendKeys(paste.getPasteCode());
+        pasteName.sendKeys(paste.getPasteName());
+        pasteExpirationContainer.click();
+        actions().moveToElement(pasteExpirationContainer)
+                .sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).perform();
+        if (paste.getSyntaxHighlighting() != null) {
+            executeJavaScript("arguments[0].scrollIntoView();", codeArea);
+            syntaxHighlightingContainer.click();
+            syntaxHighlightingInput.sendKeys(paste.getSyntaxHighlighting());
+            syntaxHighlightingInput.sendKeys(Keys.ENTER);
+        }
+        createNewPasteButton.shouldBe(Condition.visible);
+        return new PastebinCreatedPage();
+    }
+}
+
---
+++ src/test/java/tasks/page/pastebinselenide/PastebinCreatedPage.java
@@ -0,0 +1,4 @@
+package tasks.page.pastebinselenide;
+
+public class PastebinCreatedPage {
+}
